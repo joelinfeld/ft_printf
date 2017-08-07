3ddbbfbf6ffa4954d80bcc2c67a5f7a8d2d55e93
Joel <joelinfeld@gmail.com>
2017-08-07 15:30:15 -0700

    printf

--- s.c
+++ s.c
@@ -5,7 +5,7 @@ char	*s(va_list args, char c, int mod)
 	char	*str;
 	if (mod == 0 && c != 'S')
 		str = va_arg(args, char*);
-	//if (mod == 1 || c == 'S')
-	//	str = (char*)va_arg(args, wchar_t*);
+	if (mod == 1 || c == 'S')
+		str = (char*)va_arg(args, wchar_t*);
 	return (str);
 }
