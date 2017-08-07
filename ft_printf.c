f50fc9b14a3417435826dc37460bbc5bc7a5e676
Joel <joelinfeld@gmail.com>
2017-08-07 14:43:02 -0700

    printf

--- ft_printf.c
+++ ft_printf.c
@@ -42,14 +42,17 @@ int		handleprecision(t_flag *flag, int len)
 	char *str2;
 
 	i = 0;
+	/*
 	if (flag->precision && flag->c == 's')
 	{
 		if (flag->precision < len)
 		{
+			str2 = ft_strndup(flag->str, flag->precision);
 			flag->str[flag->precision] = '\0';
 			len = flag->precision;
 		}		
 	}
+	*/
 	else if (flag->precision > len && flag->c != 's' && flag->c != 'c')
 	{
 		i = -1;
