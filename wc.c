1ffb7f3ac15b6c8429aa052eb09f0e2b9df31646
Joel INFELD <jinfeld@e1z2r5p29.42.us.org>
2017-08-22 15:33:00 -0700

    wc

--- wc.c
+++ wc.c
@@ -6,7 +6,7 @@
 /*   By: jinfeld <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/22 14:00:55 by jinfeld           #+#    #+#             */
-/*   Updated: 2017/08/22 14:29:07 by jinfeld          ###   ########.fr       */
+/*   Updated: 2017/08/22 15:32:36 by jinfeld          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ wchar_t		*wc(va_list args, char c, int mod)
 {
 	wchar_t		*str;
 
-	str = NULL;
+	str = malloc(sizeof(wchar_t));
 	if ((mod = 1 && c == 'c') || (mod = 0 && c == 'C'))
 		*str = va_arg(args, wchar_t);
 	return (str);	
