6b991f3479af31c05aea5a3221a95b47d04818c9
Joel INFELD <jinfeld@e1z2r3p4.42.us.org>
2017-09-14 17:45:11 -0700

    printf

--- ft_printf.c
+++ ft_printf.c
@@ -72,8 +72,8 @@ int		typeselect(va_list args, char *str)
 		}
 	}
 		chars = demprintz(flag);
-	if (flag.str && (flag.edit || (flag.c != 's' && flag.c != '%')))
-		//ddelete(&(flag.str));
+	if (flag.str && ((flag.c != 's' && flag.c != '%')))
+		ddelete(&(flag.str));
 	if (flag.c == 'C')
 		free(flag.wstr);
 	return (chars);
