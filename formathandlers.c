4bc08e553d5dcbfbe445beb3aa7a26cb1dbe6259
Joel INFELD <jinfeld@e1z2r3p4.42.us.org>
2017-09-14 17:38:29 -0700

    printf

--- formathandlers.c
+++ formathandlers.c
@@ -60,8 +60,6 @@ void	handleprecisionhelp(t_flag *flag, int *len, char *str, char *str2)
 		if (flag->precision < *len)
 		{
 			str = ft_strndup(flag->str, flag->precision);
-			//if(flag->str)
-			//	ft_strclr(flag->str);
 			flag->str = ft_strdup(str);
 			flag->str[flag->precision] = '\0';
 			*len = ft_strlen(flag->str);
@@ -76,7 +74,7 @@ void	handleprecisionhelp(t_flag *flag, int *len, char *str, char *str2)
 		while (++i < flag->precision - *len)
 			str[i] = '0';
 		str2 = ft_strjoin(str, flag->str);
-		//ddelete(&(flag->str));
+		ddelete(&(flag->str));
 		flag->str = ft_strdup(str2);
 		*len = ft_strlen(flag->str);
 		flag->edit = 1;
